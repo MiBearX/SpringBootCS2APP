bc7f4c8064bf6a62f94ba849c8ffa3c212b42a6a
michaelxiong <michaelyangxiong@gmail.com>
2025-09-02 21:14:04 -0600

    Added single skin page functionality.

--- src/main/java/com/csapp/inventory/SkinsController.java
+++ src/main/java/com/csapp/inventory/SkinsController.java
@@ -9,6 +9,7 @@ import com.csapp.inventory.SkinRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -28,5 +29,10 @@ public class SkinsController {
         } else {
             return repo.searchByNameOrGun(q, pageable);
         }
+    }
+
+    @GetMapping("/{id}")
+    public Skin getSingleSkin(@PathVariable Integer id) {
+        return repo.findById(id).orElse(null);
     }
 }
